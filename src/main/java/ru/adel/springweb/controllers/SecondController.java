deb14fd568c7ee32091c519b734aa02975fd353a
Saqqqner <rikardofik@gmail.com>
2023-01-08 14:13:42 +0300

    v1

--- src/main/java/ru/adel/springweb/controllers/SecondController.java
+++ src/main/java/ru/adel/springweb/controllers/SecondController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "hello-adel";
+public class SecondController {
+
+    @GetMapping("/exit")
+    public String exit() {
+        return "second/exit";
     }
 }
