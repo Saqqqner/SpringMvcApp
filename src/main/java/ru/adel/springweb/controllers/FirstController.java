50c0ea70b627f5a93d3069a371790914257ee978
Saqqqner <rikardofik@gmail.com>
2023-01-09 00:46:10 +0300

    v2

--- src/main/java/ru/adel/springweb/controllers/FirstController.java
+++ src/main/java/ru/adel/springweb/controllers/FirstController.java
@@ -1,15 +1,44 @@
 package ru.adel.springweb.controllers;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname, Model model) {
+//        System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("message","Hello " + name + " " + surname);
         return "first/hello";
+    }
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a ,@RequestParam ("b") int b ,
+                             @RequestParam("action") String action ,Model model){
+        double result;
+        switch (action){
+            case "Plus":
+                result = a + b ;
+                break;
+            case "Minus":
+                result = a - b ;
+                break;
+            case "Multi":
+                result = a * b ;
+                break;
+            case "Divis":
+                result= a / (double) b;
+                break;
+            default:
+                result= 0;
+                break;
+        }
+        model.addAttribute("result",result);
+        return "first/calculator";
     }
 
     @GetMapping("/goodbye")
